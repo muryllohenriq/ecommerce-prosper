63e7a8514283c6a29192f60fdac8a4fb5c034fb9
Muryllo <muryllo.henrique@hotmail.com>
2023-10-08 21:51:42 -0300

    Finished first part

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -21,83 +21,181 @@ export default function Cadastro() {
     privacyPolicy: ''
   })
 
+  const [errors, setErrors] = useState({})
+
+  const handleChange = (e: any) => {
+    const {name, value} = e.target;
+    setFormData({
+      ...formData, [name] : value
+    })
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    const validationErrors = {}
+
+    if(!formData.name.trim()) {
+      validationErrors.name = "Nome é necessário"
+    }
+
+    if(!formData.email.trim()) {
+      validationErrors.email = "E-mail é necessário"
+    } else if(!/\S+@\S+\.\S+/.test(formData.email)){
+      validationErrors.email = "E-mail não válido"
+    }
+
+    if(!formData.telephone.trim()) {
+      validationErrors.telephone = "Telefone é necessário"
+    }
+
+    if(!formData.address.trim()) {
+      validationErrors.address = "Endereço é necessário"
+    }
+
+    if(!formData.zip.trim()) {
+      validationErrors.zip = "Cep é necessário"
+    }
+
+    if(!formData.number.trim()) {
+      validationErrors.number = "Número é necessário"
+    }
+
+    if(!formData.complement.trim()) {
+      validationErrors.complement = "Complemento é necessário"
+    }
+
+    if(!formData.neighborhood.trim()) {
+      validationErrors.neighborhood = "Bairro é necessário"
+    }
+
+    if(!formData.city.trim()) {
+      validationErrors.city = "Cidade é necessário"
+    }
+
+    if(!formData.state.trim()) {
+      validationErrors.state = "Estado é necessário"
+    }
+
+    if(!formData.password.trim()) {
+      validationErrors.password = "Senha é necessária"
+    } else if(formData.password.length < 8){
+      validationErrors.password = "Senha precisa conter mais que 8 caracteres."
+    }
+
+    if(!formData.confirmPassword.trim()) {
+      validationErrors.confirmPassword = "Confirmar senha é necessário"
+    } else if(formData.confirmPassword !== formData.password){
+      validationErrors.confirmPassword = "Senhas precisam ser iguais"
+    }
+
+    setErrors(validationErrors)
+
+    if(Object.keys(validationErrors).length === 0) {
+      alert("Cadastro criado com sucesso!")
+    }
+  }
+
   return (
     <>
       <StyledCard>
         <Link href="/">Home</Link>
         <h4>Cadastro</h4>
+        <form onSubmit={handleSubmit}>      
         <div>
           <label>Nome completo</label>
           <input
             type="text"
             name="name"
             onChange={handleChange} />
+            {errors.name && <span>{errors.name}</span>}
         </div>
         <div>
           <label>E-mail</label>
           <input
             type="email"
-            name="email" />
+            name="email"
+            onChange={handleChange} />
+            {errors.email && <span>{errors.email}</span>}
         </div>
         <div>
           <label>Telefone</label>
           <input
             type="text"
-            name="telephone" />
+            name="telephone"
+            onChange={handleChange} />
+            {errors.telephone && <span>{errors.telephone}</span>}
         </div>
         <div>
           <label>Endereço</label>
           <input
             type="text"
-            name="address" />
+            name="address"
+            onChange={handleChange} />
+            {errors.address && <span>{errors.address}</span>}
         </div>
         <div>
           <label>CEP</label>
           <input
             type="text"
-            name="zip" />
+            name="zip"
+            onChange={handleChange} />
+            {errors.zip && <span>{errors.zip}</span>}
         </div>
         <div>
           <label>Número</label>
           <input
             type="number"
-            name="number" />
+            name="number"
+            onChange={handleChange} />
+            {errors.number && <span>{errors.number}</span>}
         </div>
         <div>
           <label>Complemento</label>
           <input
             type="text"
-            name="complement" />
+            name="complement"
+            onChange={handleChange} />
+            {errors.complement && <span>{errors.complement}</span>}
         </div>
         <div>
           <label>Bairro</label>
           <input
             type="text"
-            name="neighborhood" />
+            name="neighborhood"
+            onChange={handleChange} />
+            {errors.neighborhood && <span>{errors.neighborhood}</span>}
         </div>
         <div>
           <label>Cidade</label>
           <input
             type="text"
-            name="city" />
+            name="city"
+            onChange={handleChange} />
+            {errors.city && <span>{errors.city}</span>}
         </div>
         <div>
           <label>Estado</label>
           <input
             type="text"
-            name="state" />
+            name="state"
+            onChange={handleChange} />
+            {errors.state && <span>{errors.state}</span>}
         </div>
         <div>
           <label>Senha</label>
           <input
             type="password"
-            name="password" />
+            name="password"
+            onChange={handleChange} />
+            {errors.password && <span>{errors.password}</span>}
         </div>
         <div>
           <label>Confirmar senha</label>
           <input
             type="password"
-            name="confirmPassword" />
+            name="confirmPassword"
+            onChange={handleChange} />
+            {errors.confirmPassword && <span>{errors.confirmPassword}</span>}
         </div>
         <div>
           <label>Políticas de privacidade</label>
@@ -105,7 +203,8 @@ export default function Cadastro() {
             type="checkbox"
             name="privacyPolicy" />
         </div>
-        <button>Cadastrar</button>
+        <button type="submit">Cadastrar</button>
+        </form>
       </StyledCard>
     </>
   )
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,16 +4,24 @@ import { StyledCard } from "@/components/Card.style";
 import Link from "next/link";
 
 export default function Login() {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log('Login feito com sucesso!');
+  }
+
   return (
     <>
       <StyledCard>
       <Link href="/">Home</Link>
       <h4>Login</h4>
+      <form onSubmit={handleSubmit}>
       <label>E-mail</label>
       <input type="text" />
       <label>Senha</label>
       <input type="password" />
-      <button>Login</button>
+      <button type="submit">Login</button>
+      </form>
       </StyledCard>
     </>
   )
