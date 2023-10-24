190c9258ad6033058cf0db7566a38ef245d43b5f
Muryllo <muryllo.henrique@hotmail.com>
2023-10-24 20:06:33 -0300

    Conectando com a api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,27 @@
 
 import { StyledLi } from "@/components/Li.style";
 import { StyledUl } from "@/components/Ul.style";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/products?limit=8");
+      const data = await response.json();
+
+      setProducts(data.products);
+    } catch (error) {
+      throw new Error(JSON.stringify(error))
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <main>
       <StyledUl>
@@ -15,6 +33,9 @@ export default function Home() {
           <Link href="cadastro">Cadastro</Link>
         </StyledLi>
       </StyledUl>
+      {products.map((item) => (
+        <div>{item.title}</div>
+      ))}
     </main>
   )
 }
